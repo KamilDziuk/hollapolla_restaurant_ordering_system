8b9be4365f9c4e214f39b067df84a22c8f31a382
kamildziuk <129060752+KamilDziuk@users.noreply.github.com>
2024-11-17 14:40:13 +0100

    Update orders.js
    Improving code readability;

--- js/orders.js
+++ js/orders.js
@@ -3,102 +3,92 @@ let orderSummaryBackground = document.querySelector(".orderSummaryBackground");
 let orderCodeText = document.querySelector(".orderCodeText");
 let deleteOrder = document.querySelector('.deleteOrder');
 let position = document.querySelectorAll('.position');
-
+let displayInfo = document.querySelector('.display');
+let quantity  = 1;
+let orderCode;
+const textStatus = document.querySelector('.textStatus');
+displayInfo.style.display = "none";
 deleteOrder.style.display = "none";
-// let totalpayment = document.querySelector(".totalpayment");
-
-// let priceDish = 0
-// totalpayment.innerHTML = priceDish;
-let orderCode ;
 contenerOrderInfo.style.display = "none";
 orderSummaryBackground.style.display = "none";
 
+
+
+
+
  let orderCounter = setInterval(() => {
- orderCode = Math.floor(Math.random() * 100);
-
+orderCode = Math.floor(Math.random() * 100);
 orderCodeText.innerHTML = `Order code: ${orderCode}`;
-
 },1000);
 
 
-let displayInfo = document.querySelector('.display');
-displayInfo.style.display = "none";
 
 
-const textStatus = document.querySelector('.textStatus');
+
+
 
 
 
 
 function handlePositionClick(orderNumber) {
-  position[orderNumber -1].style.display = "none";
-
-  displayInfo.style.display = "none";
-  deleteOrder.style.display = "block";
-  contenerOrderInfo.style.display = "block";
-
-  const positionElement = document.createElement('div');
-  const quantityElement = document.createElement('div');
-  const addQuantityElement = document.createElement('div');
-  const deleteQuantityElement = document.createElement('div');
-  quantityElement.id = `quantity${orderNumber}`;
-
-     quantityElement.innerHTML = `1`;
-
-
- 
-  
-
-  addQuantityElement.id = `${orderNumber}`;
-  deleteQuantityElement.id = `${orderNumber}`;
-
-  positionElement.innerHTML = `Numbers order: <input type="text" id="numbersOrder" name="numbersOrder" placeholder="Numbers order" value=" ${orderNumber}">`;
-
-   handleQuantityClick = () => {
+position[orderNumber -1].style.display = "none";
+displayInfo.style.display = "none";
+deleteOrder.style.display = "block";
+contenerOrderInfo.style.display = "block";
+const positionElement = document.createElement('div');
+const quantityElement = document.createElement('div');
+const addQuantityElement = document.createElement('div');
+const deleteQuantityElement = document.createElement('div');
+quantityElement.id = `quantity${orderNumber}`;
+quantityElement.innerHTML = `Quantity <div> 1 </div>`;
+addQuantityElement.id = `   Quantity <div> ${orderNumber} </div>`;
+deleteQuantityElement.id = `   Quantity <div> ${orderNumber} </div>`;
+positionElement.innerHTML = `Numbers order <input type="text" id="numbersOrder" name="numbersOrder" placeholder="Numbers order" value=" ${orderNumber}">`;
 
 
+addQuantityElement.addEventListener("click", () => {
+let quantityId1 =  document.querySelector(`#quantity${orderNumber}`) ;
+let quantitValue1 = quantityId1.innerHTML = `Quantity <div> ${quantity ++} </div>`
 
+if(quantityId1.innerHTML <= 0)
+  {
+  quantityId1.innerHTML = ` Quantity <div> ${1} </div>`;
+  }
+});
 
+ deleteQuantityElement.addEventListener("click", () => {
 
-
-
-  };
-  let quantity  = 1;
-  addQuantityElement.addEventListener("click", () => {
- 
-    let quantityId1 =  document.querySelector(`#quantity${orderNumber}`) ;
-    let quantitValue1 = quantityId1.innerHTML =  quantity ++
-
-
-    if(quantityId1.innerHTML <= 0)
-      {
-        quantityId1.innerHTML = 1;
-      }
-  
-
-
-
-  })
-  deleteQuantityElement.addEventListener("click", () => {
-
-    let quantityId =  document.querySelector(`#quantity${orderNumber}`) ;
-    let quantitValue = quantityId.innerHTML =  quantity --
-    if(quantityId.innerHTML <= 0)
+ let quantityId =  document.querySelector(`#quantity${orderNumber}`) ;
+ let quantitValue = quantityId.innerHTML =  `Quantity <div> ${quantity --} </div>`
+if(quantityId.innerHTML <= 0)
     {
-      quantityId.innerHTML = 1;
+      quantityId.innerHTML = ` Quantity <div> ${1} </div>`;
     }
-   
-   
+  });
+  
+  
+  deleteOrder.addEventListener("click", () =>
+    {
+      position[orderNumber -1].style.display = "block";
+      document.querySelector('.numbersOrderLocation').removeChild(positionElement);
+      document.querySelector('.quantityLocation').removeChild(quantityElement);
+      document.querySelector('.addQuantity').removeChild(addQuantityElement);
+      document.querySelector('.deleteQuantity').removeChild(deleteQuantityElement);
+      deleteOrder.style.display = "none";
+      contenerOrderInfo.style.display = "none";
+    
+    });
+    
 
 
+addQuantityElement.innerHTML = `<br><br>+<br><br>`;
+deleteQuantityElement.innerHTML = `<br><br>-<br><br>`;  
 
-  })
-addQuantityElement.innerHTML = `+`;
-deleteQuantityElement.innerHTML = `_`;
-  document.querySelector('.numbersOrderLocation').appendChild(positionElement);
-  document.querySelector('.quantityLocation').appendChild(quantityElement);
-  document.querySelector('.addQuantity').appendChild(addQuantityElement);
-  document.querySelector('.deleteQuantity').appendChild(deleteQuantityElement);
+
+document.querySelector('.numbersOrderLocation').appendChild(positionElement);
+document.querySelector('.quantityLocation').appendChild(quantityElement);
+document.querySelector('.addQuantity').appendChild(addQuantityElement);
+document.querySelector('.deleteQuantity').appendChild(deleteQuantityElement);
 
     
 document.querySelector('#ordersForm').addEventListener('submit', async function(e) {
@@ -123,10 +113,10 @@ document.querySelector('#ordersForm').addEventListener('submit', async function(
                       Telefon: ${phone}<br>
                       Email: ${email}<br>
                       Adres: ${addres}<br>
-                         Wiadomość od klienta: ${message}
+                       Wiadomość od klienta: ${message}
                 <br><br>
                        Danie numer:  ${orderNumber}<br>
-                         Ilość: ${ document.querySelector('#quantity').value}<br><br>
+                         Ilość: ${quantity}<br><br>
  
 
     Zgody: <br><br>
@@ -164,29 +154,17 @@ zgoda3: I consent to the processing by HollaPolla of my personal data contained 
 });
 
 
-
-deleteOrder.addEventListener("click", () =>
-{
-  position[orderNumber -1].style.display = "block";
-  document.querySelector('.numbersOrderLocation').removeChild(positionElement);
-  document.querySelector('.quantityLocation').removeChild(quantityElement);
-  document.querySelector('.addQuantity').removeChild(addQuantityElement);
-  document.querySelector('.deleteQuantity').removeChild(deleteQuantityElement);
-  deleteOrder.style.display = "none";
-  contenerOrderInfo.style.display = "none";
- 
-});
+};
 
 
-  
-}
 
 
 
 document.querySelectorAll('.position').forEach((position, index) => {
-  position.addEventListener("click", () => handlePositionClick(index +1));
+  position.addEventListener("click", () => handlePositionClick(   index +1 ));
 
 
 });
+
 
 
