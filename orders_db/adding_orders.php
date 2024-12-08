784170ad14d1cdf8ee9ecc67933e7ea4698a8601
kamildziuk <kontakt@kamildziuk.pl>
2024-12-08 16:32:39 +0100

    Beta orders db 1.2.1
    Improving the security of database connections and sending query

--- orders_db/adding_orders.php
+++ orders_db/adding_orders.php
@@ -14,14 +14,26 @@ $message= $_POST['message'];
 try {
     
 
-$sql = "INSERT INTO orders (codeNumber, orderNumber, quantity, first_name, email, addres, order_date, order_time, message) 
-VALUES (  '$codeNumber', '$orderNumber','$quantity', '$first_name', '$email', '$addres','$order_date' ,'$order_time', '$message')";
- $stmt = $pdo->prepare($sql);
+    $sql = "INSERT INTO orders (codeNumber, orderNumber, quantity, first_name, email, addres, order_date, order_time, message) 
+    VALUES (  :codeNumber, :orderNumber,:quantity, :first_name, :email, :addres, :order_date ,:order_time, :message)";
+      $stmt = $pdo->prepare($sql);
+     
+    $stmt -> bindParam(":codeNumber", $codeNumber);
+    $stmt -> bindParam(":orderNumber", $orderNumber);
+    $stmt -> bindParam(":quantity", $quantity);
+    $stmt -> bindParam(":first_name", $first_name);
+    $stmt -> bindParam(":email", $email);
+    $stmt -> bindParam(":addres", $addres);
+    $stmt -> bindParam(":order_date", $order_date);
+    $stmt -> bindParam(":order_time", $order_time);
+    $stmt -> bindParam(":message", $message);
+   
+
  
  $stmt->execute();
 
 
- $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 
  $stmt = null;
